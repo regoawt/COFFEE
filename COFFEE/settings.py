5119eb032d987007588b38e8da7d979586862e3c
Armand Rego <armandrego@googlemail.com>
2021-02-01 20:36:17 +0000

    Deployment changes

--- COFFEE/settings.py
+++ COFFEE/settings.py
@@ -147,5 +147,5 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-#STATIC_ROOT = '/home/armandrego/coffee/static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = '/home/hone/COFFEE/static/'
+MEDIA_ROOT = '/home/hone/COFFEE/media/'
