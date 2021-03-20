c44e56fde484ae00f167684d95a493a243099ea0
Armand Rego <armandrego@googlemail.com>
2021-03-20 18:53:42 +0000

    Improve session form duration widget.

--- main/forms.py
+++ main/forms.py
@@ -2,8 +2,33 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Question, LikertAnswer, YesNoAnswer, PlainTextAnswer, Questionnaire, Session, Resource, FiveScaleAnswer
-from bootstrap_datepicker_plus import DateTimePickerInput
+from bootstrap_datepicker_plus import DateTimePickerInput, TimePickerInput
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from datetime import timedelta
+
+# Inspired by multiwidget example here https://docs.djangoproject.com/en/3.1/ref/forms/widgets/#styling-widget-instances
+class DurationSelectorWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        hours = [(hour, hour) for hour in range(0, 13)]
+        minutes = [(minute, minute) for minute in [0,15,30,45]]
+        widgets = [
+            forms.NumberInput(attrs=attrs),
+            forms.NumberInput(attrs=attrs),
+        ]
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if isinstance(value, timedelta):
+            return [value.hour, value.minute, 0]
+        elif isinstance(value, str):
+            hour, minute, second = value.split(':')
+            return [second, minute, hour]
+        return [None, None, None]
+
+    def value_from_datadict(self, data, files, name):
+        hour, minute = super().value_from_datadict(data, files, name)
+        # DateField expects a single string that it can parse into a date.
+        return '{}:{}:{}'.format(hour, minute, '00')
 
 
 class NewUserForm(UserCreationForm):
@@ -86,6 +111,7 @@ class SessionForm(forms.ModelForm):
         model =  Session
         fields = ('name','start_datetime','duration','type','additional_tutors','questionnaire')
         widgets = {'start_datetime': DateTimePickerInput(),
+                    'duration':DurationSelectorWidget(attrs={'class': 'browser-default'}),
                     'type':forms.Select(attrs={'class': 'browser-default'}),
                     'additional_tutors': forms.SelectMultiple(attrs={'class': 'browser-default'}),
                     'questionnaire': forms.Select(attrs={'class': 'browser-default'}),
